d64ef27d4825f8dff59244a3666b7362aba5abae
Fernando Souza <fefso1909@gmail.com>
2023-12-07 16:54:46 -0300

    Solução do Dia 1

---
+++ Solutions/Day1.cs
@@ -0,0 +1,79 @@
+﻿namespace Solutions;
+
+public class Day1
+{
+    private static readonly Dictionary<string, char> s_wordMap = new()
+    {
+        { "zero", '0' },
+        { "one", '1' },
+        { "two", '2' },
+        { "three", '3' },
+        { "four", '4' },
+        { "five", '5' },
+        { "six", '6' },
+        { "seven", '7' },
+        { "eight", '8' },
+        { "nine", '9' }
+    };
+
+    public static int SumOfCalibrationValues(StreamReader input)
+    {
+        var total = 0;
+        while (!input.EndOfStream)
+        {
+            total += ParseValue(input.ReadLine());
+        }
+        
+        return total;
+    }
+
+    private static int ParseValue(string? line)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            return 0;
+        }
+        
+        char first = default;
+        char last = default;
+        
+        for (var i = 0; i < line.Length; i++)
+        {
+            var current = FetchDigit(line[i..]);
+            if (current is null)
+            {
+                continue;
+            }
+
+            if (first == default)
+            {
+                first = current.Value;
+            }
+
+            last = current.Value;
+        }
+
+        var calibrationValueString = $"{first}{last}";
+        var calibrationValue = int.Parse(calibrationValueString);
+        
+        return calibrationValue;
+    }
+
+    private static char? FetchDigit(string line)
+    {
+        if (char.IsDigit(line[0]))
+        {
+            return line[0];
+        }
+
+        foreach (var pair in s_wordMap)
+        {
+            if (line.StartsWith(pair.Key))
+            {
+                return pair.Value;
+            }
+        }
+
+        return default;
+    }
+}
---
+++ UnitTests/Day1/Day1Tests.cs
@@ -0,0 +1,34 @@
+﻿using Shouldly;
+
+namespace UnitTests.Day1;
+
+public class Day1Tests
+{
+    [Fact]
+    public void SumOfCalibrationValues_Using1stExample_ShouldBe142()
+    {
+        // arrange
+        var subject = new Solutions.Day1();
+        var file = File.OpenText("Day1/1st-example.txt");
+        
+        // act
+        var result = Solutions.Day1.SumOfCalibrationValues(file);
+        
+        // assert
+        result.ShouldBe(142);
+    }
+    
+    [Fact]
+    public void SumOfCalibrationValues_Using2ndExample_ShouldBe281()
+    {
+        // arrange
+        var subject = new Solutions.Day1();
+        var file = File.OpenText("Day1/2nd-example.txt");
+        
+        // act
+        var result = Solutions.Day1.SumOfCalibrationValues(file);
+        
+        // assert
+        result.ShouldBe(281);
+    }
+}
